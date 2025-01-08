bee5f2e1289d421924be55bee0bd758436cc8985
WallaceVidoto <wallacevidoto.tech@gmail.com>
2025-01-07 21:49:49 -0300

    modified:   src/utils/JwtUtil.ts

--- src/utils/JwtUtil.ts
+++ src/utils/JwtUtil.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export class JwtUtil {
   private static JWT_SECRET = process.env.JWT;
 
-  public static GenerateToken(payload: object, expiresIn: string = "1h") {
+  public static GenerateToken(payload: object, expiresIn: string = "1d") {
     
     if (!this.JWT_SECRET) {
       throw new Error("JWT_SECRET não está definido");
